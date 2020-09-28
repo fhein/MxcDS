8e142bbd9fff7ca4c8558654e460e54b6ec5436d
frank.hein <frank.hein@maxence.de>
2020-09-28 20:36:38 +0200

    Work in progress. Started Backend Order GUI modifications. Prod Configuration changes.

--- Resources/views/backend/mxc_dsi_order/view/list/list.js
+++ Resources/views/backend/mxc_dsi_order/view/list/list.js
@@ -19,7 +19,7 @@ Ext.define('Shopware.apps.MxcDsiOrder.view.list.List', {
   getMxcDsiColumn: function (value, metaData, record) {
     let background = record.raw.mxcbc_dsi_bullet_background_color;
     let title = record.raw.mxcbc_dsi_bullet_title;
-    if (background === undefined) return '';
+    if (background === undefined) return '<div>&nbsp</div>';
     return '<div style="width:16px;height:16px;background:' + background
       + ';color:white;margin: 0 auto;text-align:center;border-radius: 4px;padding-top: 2px;" ' +
       'title="' + title +'">&nbsp</div>';
